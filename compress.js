1373ff145a4c58514b3de7d9f6b5705c7d1f360f
jeanmahai <mahai_1986@126.com>
2014-04-22 17:12:28 +0800

    sdf

--- compress.js
+++ compress.js
@@ -1,11 +1,13 @@
 /*
 node compress --folder d:/test
 
+closure-compiler:需要代理才能访问
 http://closure-compiler.appspot.com/compile
 js_code:
-compilation_level:WHITESPACE_ONLY|Simple|Advanced
+compilation_level:WHITESPACE_ONLY|SIMPLE_OPTIMIZATIONS|ADVANCED_OPTIMIZATIONS
 output_format:text
 output_info:compiled_code
+
 */
 "use strict";
 
@@ -13,6 +15,12 @@ var FS =require("fs");
 var Process=require("process");
 var Colors=require("colors");
 var Path=require("path");
+var Request=require("request");
+
+var COMPRESS_URL="http://closure-compiler.appspot.com/compile";
+var PROXY='http://127.0.0.1:8087';
+var CPMPRESS_LEVEL='SIMPLE_OPTIMIZATIONS';
+var COMPRESS_CURATION=10*1000;
 
 function unException(ex){
 	//console.info("system error \n");
@@ -55,6 +63,9 @@ function findAllJs(folder){
 			findAllJs(_path);
 		}
 		else{
+			if(new RegExp(".*\.min\.js$",'i').test(_path)) {
+				return;
+			}
 			log("js file    "+_path);
 			js.push(_path);
 		}
@@ -65,5 +76,45 @@ findAllJs(folder);
 function readJs(path){
 	return FS.readFileSync(path,"utf-8");
 }
-console.info(readJs(js[0]));
+
+function compress(){
+	var jsPath=js.shift();
+	if(!jsPath) return;
+	if(new RegExp(".*\.min\.js$",'i').test(jsPath)) {
+		compress();
+	}
+	var jsCode=readJs(jsPath);
+	log("begin compressing :    "+jsPath);
+	Request({
+		method:"POST",
+		uri:COMPRESS_URL,
+		form:{
+			js_code:jsCode,
+			compilation_level:CPMPRESS_LEVEL,
+			output_format:'text',
+			output_info:'compiled_code'
+		},
+		proxy:PROXY
+	},function(error,response,body){
+		if(!error && response.statusCode==200){
+			if(new RegExp("^Error.*","i").test(body)){
+				log(body);
+			}
+			else{
+				var dir=Path.dirname(jsPath);
+				var base=Path.basename(jsPath,'.js');
+				var min=Path.join(dir,base+".min.js");
+				log("save"+min);
+				FS.writeFileSync(min,body);
+				log("done compressed :    "+jsPath);
+			}
+			setTimeout(compress,COMPRESS_CURATION);
+		}
+		else{
+			log(error);
+		}
+	});
+}
+compress();
+
 
